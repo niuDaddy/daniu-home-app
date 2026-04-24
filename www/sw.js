11fc2fa88c0f7c54aa5590eff672b60afb2b1540
niuDaddy <wanqiang.mayhememperor@gmail.com>
2026-04-24 20:32:30 +0800

    fix: Service Worker 缓存版本化，防止升级后显示旧页面

--- www/sw.js
+++ www/sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'daniu-home-v2';
+const CACHE_NAME = 'daniu-home-v1.0.x';
 const urlsToCache = ['/'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
